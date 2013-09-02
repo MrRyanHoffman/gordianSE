6794ab3ed59c31cf4897eeac7480e67fff4f7787
Joel David Gallant <joelgallant236@gmail.com>
2013-09-02 10:46:00 -0600

    Made counting less fragile and hefty.

--- src/edu/gordian/scopes/GordianCount.java
+++ src/edu/gordian/scopes/GordianCount.java
@@ -12,7 +12,7 @@ public class GordianCount extends GordianScope {
     public void run(String name, String from, String to, String run) {
         for (int x = ((GordianNumber) getInterpreter().interpretValue(from)).getInt();
                 x <= ((GordianNumber) getInterpreter().interpretValue(to)).getInt(); x++) {
-            storage().put(name, new GordianNumber(x));
+            storage().set(name, new GordianNumber(x));
             run(run);
         }
     }
